26faea0d498e86fcb1b2df65fc0eef08025f1860
bradleyaidanjohnson <bradleyaidanjohnson@gmail.com>
2024-04-21 06:33:04 +0100

    made spraycan a working constructor function

---
+++ sprayCanTool.js
@@ -0,0 +1,21 @@
+function SprayCanTool() {
+  //set an icon and a name for the object
+  this.icon = "assets/sprayCan.jpg";
+  this.name = "SprayCan";
+  this.points = 13;
+  this.spread = 10;
+
+  this.draw = function () {
+    //if the mouse is pressed paint on the canvas
+    //spread describes how far to spread the paint from the mouse pointer
+    //points holds how many pixels of paint for each mouse press.
+    if (mouseIsPressed) {
+      for (var i = 0; i < this.points; i++) {
+        point(
+          random(mouseX - this.spread, mouseX + this.spread),
+          random(mouseY - this.spread, mouseY + this.spread)
+        );
+      }
+    }
+  };
+}
